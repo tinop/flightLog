0525f07e0950b9b3d118c236d9948ffa9c2ece64
tino <t_perucchi@hotmail.com>
2013-04-23 23:52:58 +0200

    flightTime fun :)

--- flights/models.py
+++ flights/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 import datetime
 import time
+from django.utils.timesince import timesince
+from datetime import datetime, timedelta
+
 #from datetime import datetime
 
 
@@ -38,32 +41,36 @@ class Flight(models.Model):
   fromAirport = models.CharField(max_length=4, verbose_name='from')
   toAirport = models.CharField(max_length=4, verbose_name='to')
   departure_time = models.TimeField()
-  arrival_time = models.DateTimeField()
+  arrival_time = models.TimeField()
   pic = models.ForeignKey(Pilot)
   landings = models.PositiveIntegerField()
   night = models.BooleanField(default=False)
   ifr = models.BooleanField(default=False)
   remark = models.TextField()
   #like = models.ChoiceField(choices=CHOICES, widget=forms.RadioSelect())
-    
+
   def flightTime(self):
-    FMT = '%H:%M:%S'
-    #t = datetime.strptime(self.arrival_time,'%I:%M').strftime('%H:%M')
-    t1 = self.arrival_time.date()
-    t2 = self.departure_time
-    #t3 = time.mktime(t2) - time.mktime(t1)
-    #return t2.strftime('%H:%M:%S')
-    return t2
-    #return t
-  flightTime.short_description = 'Flight Time'
-  
-  def flightTimex(self):
-    #start="10:23:00"
-    #dep = time.strptime(self.departure_time,'%I:%M')
-    #start_dt = datetime.strptime(start, '%H:%M:%S')
-    return self.arrival_time.strftime('%H:%M:%S')
-  flightTimex.short_description = 'Flight Timex'
-  
+    start = self.departure_time
+    end = self.arrival_time
+    startdelta=timedelta(hours=start.hour,minutes=start.minute,seconds=start.second)
+    enddelta=timedelta(hours=end.hour,minutes=end.minute,seconds=end.second)
+
+    # deltatime struct
+    d =(enddelta-startdelta)
+
+    return int(d.total_seconds())
+
+
+  def flightTimeFormatted(self):
+    total_seconds = self.flightTime()
+    hours, remainder = divmod(total_seconds,60*60)
+    minutes, seconds = divmod(remainder,60)
+     
+    return '{}:{}'.format(hours,minutes)
+    return '%s:%s:%s' % (d.seconds/60/60, d.seconds/60, d.seconds)
+
+
+
   
   def __unicode__(self):
-      return self.fromAirport
+    return self.fromAirport
